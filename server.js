e46ce59bed8f2b4d399b55536558c28f950b81a9
bhushano7 <bhushan2250@gmail.com>
2015-01-30 21:57:11 +0530

    update changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = require("./app_conf.js").init(express);
 var server = require('http').createServer(app).listen(5000);
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
 
 app.get('/', function (request, response) {
 	console.log('Hello Travis Page!');
